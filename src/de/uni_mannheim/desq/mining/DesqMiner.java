bc16efe82826d706b45dd80622c12c50489ca64f
Rainer Gemulla <rgemulla@uni-mannheim.de>
2016-07-14 11:19:13 +0200

    Added PatternCollector to DesqMiner

--- src/de/uni_mannheim/desq/mining/DesqMiner.java
+++ src/de/uni_mannheim/desq/mining/DesqMiner.java
@@ -4,16 +4,15 @@ import de.uni_mannheim.desq.collector.PatternCollector;
 import it.unimi.dsi.fastutil.ints.IntList;
 
 public abstract class DesqMiner {
+	// if null, patterns are mined but not collected
 	protected PatternCollector patternCollector;
-	protected boolean collectPatterns;
 	
-	DesqMiner(PatternCollector patternCollector, boolean collectPatterns) {
-		this.patternCollector = patternCollector;
-		this.collectPatterns = collectPatterns;
+	protected DesqMiner(PatternCollector patternCollector) {
+		setPatternCollector(patternCollector);
 	}
-
-	DesqMiner(PatternCollector patternCollector) {
-		this(patternCollector, patternCollector != null);
+	
+	public void setPatternCollector(PatternCollector patternCollector) {
+		this.patternCollector = patternCollector;
 	}
 	
 	/** Adds a new input sequence. The provided sequence must not be buffered by this miner. */
