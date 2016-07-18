2a6b30654f12aee2970ac067f7fe4e5c2258967c
Kaustubh Beedkar <kbeedkar@gmail.com>
2016-07-18 15:53:10 +0200

    Moved visual package to new package structure

---
+++ src/main/java/de/uni_mannheim/desq/visual/Vfst.java
@@ -0,0 +1,49 @@
+package de.uni_mannheim.desq.visual;
+
+import java.io.File;
+
+/**
+ * Vdfa.java
+ * @author Kaustubh Beedkar {kbeedkar@uni-mannheim.de}
+ */
+public class Vfst {
+	GraphViz gv = new GraphViz();
+	String type = "pdf";
+	String outfile;
+
+	public Vfst(String type, String outfile) {
+		this.type = type;
+		this.outfile = outfile;
+	}
+
+	public Vfst(String outfile) {
+		this.outfile = outfile;
+	}
+
+	public void beginGraph() {
+		gv.addln(gv.start_graph());
+		gv.addln("rankdir=\"LR\";");
+		gv.addln("node [shape=circle];");
+	}
+
+	public void endGraph() {
+		gv.addln(gv.end_graph());
+
+		File out = new File(outfile + "." + type);
+		gv.writeGraphToFile(gv.getGraph(gv.getDotSource(), type), out);
+	}
+	
+	public void addAccepted(String state) {
+		gv.add(state + " [shape=doublecircle]");
+	}
+
+	public void add(String fromState, String iLabel, String oLabel,  String toState) {
+		String edgeLabel = iLabel + ":" + oLabel;
+		add(fromState, edgeLabel, toState);
+	}
+	
+	public void add(String fromState, String edgeLabel, String toState) {
+		String dotString = fromState + " -> " + toState + " [label=\"" + edgeLabel + "\"];";
+		gv.addln(dotString);
+	}
+}
