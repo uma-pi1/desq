a8557602d90741620c7b0c53bf86610ac1e50515
ryangodwin <ryangodwin.serrao2013@vit.ac.in>
2017-02-21 18:15:37 +0100

    Update Fimi.java

--- src/main/java/de/uni_mannheim/desq/experiments/fimi/Fimi.java
+++ src/main/java/de/uni_mannheim/desq/experiments/fimi/Fimi.java
@@ -1,70 +1,54 @@
 package de.uni_mannheim.desq.experiments.fimi;
 
-import de.uni_mannheim.desq.converters.Fimi.FimiConverter;
 import de.uni_mannheim.desq.dictionary.Dictionary;
 import de.uni_mannheim.desq.examples.ExampleUtils;
 import de.uni_mannheim.desq.io.DelSequenceReader;
 import de.uni_mannheim.desq.io.SequenceReader;
 import de.uni_mannheim.desq.mining.DesqDfs;
 import de.uni_mannheim.desq.util.DesqProperties;
-import it.unimi.dsi.fastutil.ints.IntArrayList;
-import it.unimi.dsi.fastutil.ints.IntList;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 
 /**
- * Created by ryan on 29.01.17.
+ * Created by ryan on 15.02.17.
  */
 public class Fimi {
 
+    public static void implementFimi(DesqProperties minerConf, String dataPath, String dictPath) throws IOException {
+        File dataFile=new File(dataPath);
+        File dictFile=new File(dictPath);
 
-    public static void  runFimi(DesqProperties minerConf, String dataPath) throws IOException {
-        Dictionary dict= FimiConverter.loadFrom(dataPath);
+        SequenceReader dataReader = new DelSequenceReader(new FileInputStream(dataFile), false);
 
-        URL dataFile = ExampleUtils.class.getResource("/sortedFimiRepo/sort.del");
-        // load the dictionary
-
-
-        // update hierarchy
-        SequenceReader dataReader = new DelSequenceReader(dataFile.openStream(), false);
-        dict.incFreqs(dataReader);
-        dict.recomputeFids();
-        System.out.println("Dictionary with statitics:");
-        //dict.writeJson(System.out);
-        System.out.println();
-
-        //print sequences
-        System.out.println("Input sequences:");
-        dataReader = new DelSequenceReader(dataFile.openStream(), false);
+        Dictionary dict=Dictionary.loadFrom(dictFile);
         dataReader.setDictionary(dict);
-
-        IntList inputSequence = new IntArrayList();
-/*
-        while (dataReader.readAsFids(inputSequence)) {
-            System.out.println(dict.sidsOfFids(inputSequence));
-        }
-        System.out.println();
-*/
-        dataReader = new DelSequenceReader(dataFile.openStream(), false);
-        dataReader.setDictionary(dict);
-        //return runVerbose(dataReader, minerConf);
-        ExampleUtils.runVerbose(dataReader, minerConf);
+        ExampleUtils.runWithStats(dataReader, minerConf);
+        //ExampleUtils.runVerbose(dataReader, minerConf);
 
     }
 
-    public static void fimi(String dataPath) throws IOException {
-        String patternExp= "(.)[.*(.)]*";
-        int sigma =1000;
+
+    public static void setFimi(String dataPath, String dictPath,int sigma) throws IOException {
+        String patternExp= "[.*(.)]+";
+        //int sigma =10;
 
         DesqProperties conf = DesqDfs.createConf(patternExp, sigma);
         conf.setProperty("desq.mining.prune.irrelevant.inputs", true);
         conf.setProperty("desq.mining.use.two.pass", true);
-        runFimi(conf, dataPath);
+        implementFimi(conf, dataPath, dictPath);
     }
 
+    /** Run: java filename dataPath dictPath min_supp*/
     public static void main(String[] args) throws IOException {
-        String dataPath="/home/ryan/DESQ/desq-journal/desq-desqOnFimi-bdf66e7a3dc785bcb9eb8dd16138d6e2bd421c48/data/fimiRepo/retail.dat";
-        fimi(dataPath);
+        //String dataPath="data/fimi/bms/test.del";
+        //String dictPath="data/fimi/bms/test-dict.json";
+        String dataPath=args[0];
+        String dictPath=args[1];
+        int sigma=Integer.parseInt(args[2]);
+        setFimi(dataPath,dictPath,sigma);
     }
 }
