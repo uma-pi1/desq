569fcbe8c149f97e6989a4d5582f3263173fd740
Alexander Renz-Wieland <alexrzw@gmail.com>
2017-06-15 23:03:36 +0200

    Fixes the empty-output recursion prevention

--- src/main/java/de/uni_mannheim/desq/mining/DesqDfs.java
+++ src/main/java/de/uni_mannheim/desq/mining/DesqDfs.java
@@ -101,6 +101,8 @@ public final class DesqDfs extends MemoryDesqMiner {
 	 */
 	BitSet currentSpReachedWithoutOutput = new BitSet();
 
+	ObjectList<BitSet> spReachedWithoutOutput = new ObjectArrayList<>();
+
 	// -- construction/clearing ---------------------------------------------------------------------------------------
 
 	// -- helper variables for distributing --------------------------------------------------------------------------
@@ -525,8 +527,13 @@ itemState:	while (itemStateIt.hasNext()) { // loop over elements of itemStateIt;
 	 */
 	public IntSet generatePivotItems(IntList inputSequence, RelevantPositions relevantPositions) {
 		pivotItems.clear();
-		currentSpReachedWithoutOutput.clear();
 		this.inputSequence = inputSequence;
+
+		while(spReachedWithoutOutput.size() <= this.inputSequence.size()) {
+			spReachedWithoutOutput.add(new BitSet());
+		}
+		currentSpReachedWithoutOutput = spReachedWithoutOutput.get(0);
+		currentSpReachedWithoutOutput.clear();
 
 		// check whether sequence produces output at all. if yes, produce output items
 		if(useTwoPass) {
@@ -570,7 +577,12 @@ itemState:	while (itemStateIt.hasNext()) { // loop over elements of itemStateIt;
 		path.clear();
 		nfas.clear();
 
+		while(spReachedWithoutOutput.size() <= this.inputSequence.size()) {
+			spReachedWithoutOutput.add(new BitSet());
+		}
+		currentSpReachedWithoutOutput = spReachedWithoutOutput.get(0);
 		currentSpReachedWithoutOutput.clear();
+
 		if(useTwoPass) {
 			dfaStateSequence.clear();
 			if (dfa.acceptsReverse(inputSequence, dfaStateSequence, dfaInitialPos)) {
@@ -728,6 +740,8 @@ itemState:	while (itemStateIt.hasNext()) { // loop over elements of itemStateIt;
 
 			// We handle the different output label types differently
 			if(!tr.hasOutput()) { // this transition doesn't produce output
+                // an eps transition does not introduce potential pivot elements, so we simply continue recursion
+
 				// if we saw this state at this position without output (for this input sequence and for the currently
 				// expanded node) before, we do not need to process it again
 				int spIndex = pos * fst.numStates() + toState.getId();
@@ -735,7 +749,6 @@ itemState:	while (itemStateIt.hasNext()) { // loop over elements of itemStateIt;
 					// haven't seen it, so process
 					currentSpReachedWithoutOutput.set(spIndex);
 
-					// an eps transition does not introduce potential pivot elements, so we simply continue recursion
 					piStep(largestFidSeen, pos + 1, toState, level + 1, relevantPositions, currentLastIrrelevant);
 				}
 
@@ -784,8 +797,11 @@ itemState:	while (itemStateIt.hasNext()) { // loop over elements of itemStateIt;
                     // add this output label to the path and follow the path further. afterwards, remove the
 					//   output label from the path and continue with the next transition
 					path.add(ol);
+					currentSpReachedWithoutOutput = spReachedWithoutOutput.get(path.size());
+					currentSpReachedWithoutOutput.clear();
 					piStep(largestFidSeen, pos + 1, toState, level + 1, relevantPositions, currentLastIrrelevant);
 					path.remove(path.size() - 1);
+					currentSpReachedWithoutOutput = spReachedWithoutOutput.get(path.size());
 					processedPathSize = path.size();
 				}
 			}
