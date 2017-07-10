982cc5a143e43645f67883bad6d39a7ac83e593c
Kai-Arne Theis <ktheis@mail.uni-mannheim.de>
2017-07-10 20:04:22 +0200

    Added new add method using a LongArrayList

--- src/main/java/de/uni_mannheim/desq/mining/EliasGammaPostingList.java
+++ src/main/java/de/uni_mannheim/desq/mining/EliasGammaPostingList.java
@@ -5,6 +5,7 @@
  */
 package de.uni_mannheim.desq.mining;
 
+import it.unimi.dsi.fastutil.longs.LongArrayList;
 import java.util.BitSet;
 
 /**
@@ -16,9 +17,20 @@ public class EliasGammaPostingList implements IPostingList{
     private int currentPosition;
     private BitSet data;
     
+    private int freeBits;
+    private int offset;
+    private LongArrayList dataList;
+    
     public EliasGammaPostingList() {
+        // addInt()
         data = new BitSet();
         currentPosition = 0;
+        
+        // addInt2()
+        dataList = new LongArrayList();
+        dataList.add(0);
+        offset = 0;
+        freeBits = 64;
     }   
     
     @Override
@@ -55,6 +67,48 @@ public class EliasGammaPostingList implements IPostingList{
         currentPosition++;
     }
 
+    public void addInt2(int value){
+        int valueToAdd = ++value;
+        
+        byte length = (byte) (32 - Integer.numberOfLeadingZeros(valueToAdd));
+        
+        int totalLength = 2 * length - 1;
+        
+        if(freeBits >= totalLength){
+            long tmp = dataList.getLong(dataList.size() - 1);
+                        
+            dataList.set(offset, tmp |= ((long) valueToAdd) << (freeBits -= totalLength));
+            
+            if(freeBits == 0){
+                freeBits = 64;
+                offset++;
+                dataList.add(0);
+            }
+        } else {
+            if(length - 1 >= freeBits){
+                int toAdd = (length - 1) - freeBits;
+                freeBits = 64;
+                offset++;
+                
+                if(toAdd == 0){
+                    dataList.add(((long) valueToAdd) << (freeBits -= length));
+                } else {
+                    dataList.add(((long) valueToAdd) << (freeBits -= (length + toAdd)));
+                }
+            } else {
+                long tmp = dataList.getLong(offset);
+                
+                int toAdd = totalLength - freeBits;
+                dataList.set(offset, tmp |= ((long) valueToAdd) >>> (totalLength - freeBits));
+                
+                freeBits = 64;
+                offset++;
+                
+                dataList.add(((long) valueToAdd) << (freeBits -= toAdd));
+            }
+        }  
+    }
+    
     @Override
     public void newPosting() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
