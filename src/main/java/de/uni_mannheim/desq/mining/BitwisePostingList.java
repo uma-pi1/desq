3daeab1ff473e02111cfb8b8e81a4eae0f398b74
Kai-Arne Theis <ktheis@mail.uni-mannheim.de>
2017-07-25 15:32:36 +0200

    BitwisePostingList now implements AbstractPostingList and Iterator

--- src/main/java/de/uni_mannheim/desq/mining/BitwisePostingList.java
+++ src/main/java/de/uni_mannheim/desq/mining/BitwisePostingList.java
@@ -5,9 +5,7 @@
  */
 package de.uni_mannheim.desq.mining;
 
-import it.unimi.dsi.fastutil.bytes.ByteArrayList;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
-import java.util.BitSet;
 
 /**
  *
@@ -17,31 +15,15 @@ public class BitwisePostingList extends AbstractPostingList{
 
     private final IntArrayList data;
     private final IntArrayList controlData;
-    private int noPostings;
     private static byte freeBits;
-    private int currentDataByte;
-    private int currentControlDataByte;
-    private int offset;
-    private byte internalOffset;
 
     /** Constructs a new empty posting list */
     public BitwisePostingList() {
         data = new IntArrayList();
         controlData = new IntArrayList();
-        noPostings = 0;
+        data.add(0);
+        controlData.add(0);
         freeBits = 32;
-        currentDataByte = 0;
-        currentControlDataByte = 0;
-        offset = 0;
-        internalOffset = 0;
-        this.noPostings = 0;
-    }
-
-    /** Creates a new posting list with the (copied) data from the given posting list. */
-    public BitwisePostingList(BitwisePostingList postingList) {
-        this.data = new IntArrayList(postingList.data);
-        this.controlData = new IntArrayList(postingList.controlData);
-        this.noPostings = postingList.noPostings;
     }
     
     @Override
@@ -49,7 +31,7 @@ public class BitwisePostingList extends AbstractPostingList{
         
         assert value >= 0;
         //assert size() > 0;
-        value += 1; // we add the integer increased by one to distinguish it from separators;
+        //value += 1; // we add the integer increased by one to distinguish it from separators;
         
         final int b = value;
 
@@ -61,15 +43,19 @@ public class BitwisePostingList extends AbstractPostingList{
         // Check if data bits fit into the last int
         if(lengthB <= freeBits){
             
-            freeBits -= lengthB;
-            
-            // Get last int and add data bits
-            int currentDataByte2 = data.getInt(currentSize);
-            data.set(currentSize, (currentDataByte2 | b << freeBits));
-            
-            // Get last control int and add control bits
-            int currentControlDataByte2 = controlData.getInt(currentSize);
-            controlData.set(currentSize, (currentControlDataByte2 | (((1 << lengthB) - 2) << freeBits)));
+            if(lengthB == 0){
+                freeBits--;
+            } else {
+                freeBits -= lengthB;
+
+                // Get last int and add data bits
+                int currentDataByte2 = data.getInt(currentSize);
+                data.set(currentSize, (currentDataByte2 | b << freeBits));
+
+                // Get last control int and add control bits
+                int currentControlDataByte2 = controlData.getInt(currentSize);
+                controlData.set(currentSize, (currentControlDataByte2 | (((1 << lengthB) - 2) << freeBits)));
+            }
             
             // Reset variables if all 32 bits of the int are set with data bits
             if(freeBits == 0){
@@ -98,11 +84,6 @@ public class BitwisePostingList extends AbstractPostingList{
             freeBits -= (lengthB - partialCopy);
         }     
     }
-    
-    @Override
-    public void newPosting() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 
     @Override
     public int noBytes() { return data.size(); }
@@ -123,120 +104,161 @@ public class BitwisePostingList extends AbstractPostingList{
         data.trim();
     }
 
-    public int nextInt() {
-        // Get current control data integer
-        int currentData = controlData.getInt(offset);
-        
-        int gainedData = 0;
-        boolean gotData = false;
-        int partialData = 0;
-        
-        int getMask = 0;
-        byte count = 0;
-        
-        // Set 1 bit mask to the current position
-        int i = 0x80000000 >>> internalOffset;
-        do {
-            // Check if bit in control data is set or not.
-            if(0 != (currentData & i)){
-                internalOffset++;
-                
-                // Add current position to get mask
-                getMask |= i;
-                
-                i >>>= 1;
-                
-                // If the end of the control data is reached without a 0, the data bits are stored in partial data
-                if(internalOffset != 0 && internalOffset % 32 == 0){
-                    partialData = (data.getInt(offset) & getMask) >>> (32 - internalOffset);
-                    offset++;
-                    internalOffset = 0;
-                    i = 0x80000000;
-                    getMask = 0;
-                    currentData = controlData.getInt(offset);
-                }
-            } else {
-                internalOffset++;
-                
-                // Add current position to get mask
-                getMask |= i;
-                
-                i >>>= 1;
-                
-                // Get data bits from the data integer
-                gainedData = (data.getInt(offset) & getMask) >>> (32 - internalOffset);
-                
-                // If there is partial data from the previous integer it is shifted to the correct position and unified with the current data
-                if(partialData != 0){
-                    partialData <<= internalOffset;
-                    gainedData |= partialData;
-                }
-                
-                if(internalOffset != 0 && internalOffset % 32 == 0){
-                    offset++;
-                    internalOffset = 0;
-                }
-                
-                gotData = true;
-            }
-        } while (i != 0 && !gotData);
-        
-        return gainedData;
+    @Override
+    public Object getData() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    public void nextInt2(){
-        int returnValue = 0;
-        
-        boolean gotData = false;
-        boolean partialData = false;
-        
-        int partialOutput = 0;
-        
-        do{
-            // Set offset before to the current position inside the int
-            byte offsetBefore = internalOffset;
 
-            // Get the int with the current control data
-            int currentData = controlData.getInt(offset);
-            
-            /* Shift the mask to the last positon to cover zeros in the control string
-               11011101 : control data
-               11100000 : mask
-               11111101 : new control data */
-            if(offsetBefore != 0){
-                currentData |= (0x80000000) >> (offsetBefore - 1);
-            }
-            
-            // Invert control string and read number of leading zeros
-            internalOffset = (byte) (Integer.numberOfLeadingZeros(~currentData) + 1);
-            
-            // Check if the current read int is split to two ints, by checking if the internal offset is the int length
-            if(internalOffset > 32){
-                
-                int mask = ((1 << (32 - offsetBefore)) - 1);
-                
-                partialOutput = data.getInt(offset) & mask;
-                partialData = true;
-                
-                internalOffset = 0;
-                offset++;
-            } else {
-                int mask = ((1 << (internalOffset - offsetBefore)) - 1);
-                
-                returnValue = ((data.getInt(offset) >>> (32 - internalOffset)) & mask);
-                
-                if(partialData){
-                    returnValue |= (partialOutput << internalOffset);
-                } 
-                                
-                if(internalOffset == 32){
-                    internalOffset = 0;
-                    offset++;
-                }
-                
-                gotData = true;
-            } 
-        } while(!gotData);
+    @Override
+    public AbstractIterator iterator() {
+        return new Iterator(this);
     }
     
+    private class Iterator extends AbstractIterator{
+
+        private IntArrayList data;
+        private IntArrayList controlData;
+        
+        private byte internalOffset;
+        
+        public Iterator(BitwisePostingList postingList){
+            this.data = postingList.data;
+            this.controlData = postingList.controlData;
+            
+            this.internalOffset = 0;
+            this.offset = 0;
+        }
+        
+        @Override
+        int nextNonNegativeIntIntern() {
+            /*// Get current control data integer
+            int currentData = controlData.getInt(offset);
+
+            int gainedData = 0;
+            boolean gotData = false;
+            int partialData = 0;
+
+            int getMask = 0;
+            byte count = 0;
+
+            // Set 1 bit mask to the current position
+            int i = 0x80000000 >>> internalOffset;
+            do {
+                // Check if bit in control data is set or not.
+                if(0 != (currentData & i)){
+                    internalOffset++;
+
+                    // Add current position to get mask
+                    getMask |= i;
+
+                    i >>>= 1;
+
+                    // If the end of the control data is reached without a 0, the data bits are stored in partial data
+                    if(internalOffset != 0 && internalOffset % 32 == 0){
+                        partialData = (data.getInt(offset) & getMask) >>> (32 - internalOffset);
+                        offset++;
+                        internalOffset = 0;
+                        i = 0x80000000;
+                        getMask = 0;
+                        currentData = controlData.getInt(offset);
+                    }
+                } else {
+                    internalOffset++;
+
+                    // Add current position to get mask
+                    getMask |= i;
+
+                    i >>>= 1;
+
+                    // Get data bits from the data integer
+                    gainedData = (data.getInt(offset) & getMask) >>> (32 - internalOffset);
+
+                    // If there is partial data from the previous integer it is shifted to the correct position and unified with the current data
+                    if(partialData != 0){
+                        partialData <<= internalOffset;
+                        gainedData |= partialData;
+                    }
+
+                    if(internalOffset != 0 && internalOffset % 32 == 0){
+                        offset++;
+                        internalOffset = 0;
+                    }
+
+                    gotData = true;
+                }
+            } while (i != 0 && !gotData);
+
+            return gainedData;*/
+            
+            int returnValue = 0;
+        
+            boolean gotData = false;
+            boolean partialData = false;
+
+            int partialOutput = 0;
+
+            do{
+                // Set offset before to the current position inside the int
+                byte offsetBefore = internalOffset;
+
+                // Get the int with the current control data
+                int currentData = controlData.getInt(offset);
+
+                /* Shift the mask to the last positon to cover zeros in the control string
+                   11011101 : control data
+                   11100000 : mask
+                   11111101 : new control data*/ 
+                if(offsetBefore != 0){
+                    currentData |= (0x80000000) >> (offsetBefore - 1);
+                }
+
+                // Invert control string and read number of leading zeros
+                internalOffset = (byte) (Integer.numberOfLeadingZeros(~currentData) + 1);
+
+                // Check if the current read int is split to two ints, by checking if the internal offset is the int length
+                if(internalOffset > 32){
+
+                    int mask = ((1 << (32 - offsetBefore)) - 1);
+
+                    partialOutput = data.getInt(offset) & mask;
+                    partialData = true;
+
+                    internalOffset = 0;
+                    offset++;
+                } else {
+                    int mask = ((1 << (internalOffset - offsetBefore)) - 1);
+
+                    returnValue = ((data.getInt(offset) >>> (32 - internalOffset)) & mask);
+
+                    if(partialData){
+                        returnValue |= (partialOutput << internalOffset);
+                    } 
+
+                    if(internalOffset == 32){
+                        internalOffset = 0;
+                        offset++;
+                    }
+
+                    gotData = true;
+                } 
+            } while(!gotData);
+            
+            return returnValue;
+        }
+
+        @Override
+        public boolean nextPosting() {
+            if (offset >= data.size())
+                return false;
+
+            int b;
+            do {
+                b = this.nextNonNegativeIntIntern();
+                if (offset >= data.size())
+                    return false;
+            } while (b!=0);
+            return true;
+        }
+        
+    }
 }
