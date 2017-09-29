a4f5dff237597bf5e26f8eaa1b06755311cb3d59
Kai-Arne Theis <ktheis@mail.uni-mannheim.de>
2017-09-29 17:02:09 +0200

    Added a parameterized test for all posting lists

--- src/test/java/de/uni_mannheim/desq/mining/PostingListTest.java
+++ src/test/java/de/uni_mannheim/desq/mining/PostingListTest.java
@@ -1,80 +1,81 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.uni_mannheim.desq.mining;
 
+import java.util.Arrays;
+import java.util.Collection;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 /**
  *
  * @author Kai-Arne
  */
+@RunWith(Parameterized.class)
 public class PostingListTest {
     
     public AbstractPostingList postingList;
     public AbstractIterator iterator;
     public int numberOfElements;
     public int[] inputData;
-    public int[] inputData2;
+    
+    public PostingListTest(AbstractPostingList pL, AbstractIterator it) {
+        this.postingList = pL;
+        this.iterator = it;
+    }
+    
+    @Parameterized.Parameters
+    public static Collection postingLists() {
+        return Arrays.asList(new Object[][] {
+            { new BitwiseLongPostingList(), new BitwiseLongPostingList.Iterator() },
+            { new EliasGammaPostingList(), new EliasGammaPostingList.Iterator() },
+            { new IntegerPostingList(), new IntegerPostingList.Iterator()},
+            { new NewPostingList(), new NewPostingList.Iterator() },
+            { new VarBytePostingList(), new VarBytePostingList.Iterator()},
+            { new VarByteLongPostingList(), new VarByteLongPostingList.Iterator()},
+            { new VarByteLongAdvancedPostingList(), new VarByteLongAdvancedPostingList.Iterator()},
+        });
+    }
     
     @Before
     public void setUp(){
-        postingList = new VarBytePostingList();
-        numberOfElements = 100000000;
+        numberOfElements = 10000;
         inputData = new int[numberOfElements];
-        inputData2 = new int[numberOfElements];
                  
         for(int i = 0; i < numberOfElements; i++){
             int prop = (int) (Math.random() * 100);
              
             if(prop < 5){
                 inputData[i] = 0;
-                inputData2[i] = 0;
             } else if (prop >= 5 && prop < 10){
-                inputData[i] = (int) (Math.random() * 65000);
-                inputData2[i] = (int) (Math.random() * 65000);
+                inputData[i] = (int) (Math.random() * 2000000000);
             } else if (prop >= 20){
-                inputData[i] = (int) (Math.random() * 127);
-                inputData2[i] = (int) (Math.random() * 127);
+                inputData[i] = (int) (Math.random() * 4000);
             }
         }
         
-        long start = System.currentTimeMillis();
         postingList.newPosting();
         
         for(int i = 0; i < numberOfElements; i++){
+            if(i % 10 == 0){
+                postingList.newPosting();
+            }
             postingList.addNonNegativeInt(inputData[i]);
         }
-        
-        postingList.newPosting();
-        
-        for(int i = 0; i < numberOfElements; i++){
-            postingList.addNonNegativeInt(inputData2[i]);
-        }
-        System.out.println("Time init: " + (System.currentTimeMillis() - start));
-        iterator = postingList.iterator();
+       
+        iterator.reset(postingList);
     }
     
     @Test
-    public void postingListNextNonNegativeTest(){
-        for(int i = 0; i < numberOfElements; i++){
-            assertEquals(inputData[i], iterator.nextNonNegativeInt());
-        }
-    }
-    
-    @Test
-    public void postingListNextPostingTest(){
-        assertTrue(iterator.nextPosting());
+    public void postingListTest(){
+        int count = 0;
         
-        for(int i = 0; i < numberOfElements; i++){
-            assertEquals(inputData2[i], iterator.nextNonNegativeInt());
-        }
+        do{
+            while(iterator.hasNext()){
+                assertEquals(iterator.nextNonNegativeInt(), inputData[count]);
+                count++;
+            }
+        } while(iterator.hasNext());
     }
 }
