e38cf8d2cfc0372ad0e988c9d347278ba6327aef
Sascha Ulbrich <saschatu@gmx.de>
2017-11-20 09:34:43 +0100

    Performance Evaluation:  Fixing iterations, further adjustments

--- src/main/java/de/uni_mannheim/desq/experiments/MetricLogger.java
+++ src/main/java/de/uni_mannheim/desq/experiments/MetricLogger.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -19,6 +20,7 @@ public class MetricLogger {
 
     public enum Metric { //Define metrics and their order
         //Parameters
+        StartTimestamp,
         NumberDictionaryItems, NumberInputSequences, AvgLengthInputSequences,
         NumberPatExItems, NumberDistinctPatExItems,
         //Runtime Metrics
@@ -39,7 +41,7 @@ public class MetricLogger {
     private MetricLogger() {
         this.currentIteration = -1;
         this.metrics = new HashMap<>();
-        //Init iterartion lists
+        //Init iteration lists
         for(Metric m: Metric.values()){
             metrics.put(m,new HashMap<>());
         }
@@ -51,15 +53,15 @@ public class MetricLogger {
     }
 
     //next iteration
-    public void nextIteration(){
+    public void startIteration(){
         this.currentIteration++;
+        metrics.get(Metric.StartTimestamp).put(currentIteration, Instant.now().getEpochSecond());
     }
 
     // Time measurements
     public void start(Metric metric){
         metrics.get(metric).put(currentIteration,System.nanoTime());
     }
-
 
     public String stop(Metric metric){
         long time = TimeUnit.NANOSECONDS.toMillis(
@@ -78,12 +80,16 @@ public class MetricLogger {
         return value;
     }*/
 
-
     public void writeResult(String file){
+        writeResult(file,";");
+    }
+
+    public void writeResult(String file, String separator){
         StringBuilder headerBuilder = new StringBuilder();
         headerBuilder.append("Metric");
         for(int i = 0; i <= currentIteration; i++){
-            headerBuilder.append(", Iteration");
+            headerBuilder.append(separator);
+            headerBuilder.append("Iteration");
             headerBuilder.append(i);
         }
 
@@ -92,17 +98,22 @@ public class MetricLogger {
         lines.add(headerBuilder.toString());
         //collect data
         for(Metric m: Metric.values()){
-            if(metrics.containsKey(m)) {
-                StringBuilder lineBuilder = new StringBuilder();
-                lineBuilder.append(m.toString());
-                for(Long value: metrics.get(m).values()){
-                    lineBuilder.append(",");
-                    lineBuilder.append(value);
+            StringBuilder lineBuilder = new StringBuilder();
+            lineBuilder.append(m.toString());
+            Map<Integer,Long> iterations = metrics.get(m);
+            //for(Long value: metrics.get(m).values()){
+            for(int i = 0; i <= currentIteration; i++){
+                lineBuilder.append(separator);
+                if(iterations.containsKey(i)){
+                    lineBuilder.append(iterations.get(i));
+                }else{
+                    lineBuilder.append("-");
                 }
-                lines.add(lineBuilder.toString());
             }
+            lines.add(lineBuilder.toString());
         }
 
+        //Save the data
         Path path = Paths.get(file);
         try {
             Files.write(path, lines, Charset.forName("UTF-8"), StandardOpenOption.CREATE);
